fb6ee35e478604b0644f127ca413571ece7f2d06
DesignSupply <ogawa@designsupply-web.com>
2021-06-13 07:01:27 +0900

    [update] 各種関数のファイルを分割

--- functions.php
+++ functions.php
@@ -1,24 +1,25 @@
 <?php
 
-  // 各種設定関数
+  // 各種処理
   require_once('functions/actions.php');
+  require_once('functions/filters.php');
 
   // 投稿データ・クエリ操作
   require_once('functions/models.php');
 
-  // ウィジェット出力
-  require_once('functions/widgets.php');
-
-  // コメントコールバック
-  require_once('functions/comment.php');
-
   // SEOメタ情報出力
   require_once('functions/seo.php');
 
-  // メール送信関数
+  // ウィジェット出力
+  require_once('functions/widgets.php');
+
+  // メール送信処理
   require_once('functions/mail.php');
   
   // REST API設定
   require_once('functions/api.php');
+
+  // コメント入出力
+  require_once('functions/comment.php');
 
 ?>
--- functions/actions.php
+++ functions/actions.php
@@ -32,13 +32,6 @@
   }
   add_action('after_setup_theme', 'output_title');
 
-  // タイトルタグ区切り文字変更
-  function rewrite_title_separator($separator) {
-    $separator = '|';
-    return $separator;
-  }
-  add_filter('document_title_separator', 'rewrite_title_separator');
-
   // RSS用リンク出力
   add_theme_support('automatic-feed-links');
 
@@ -62,12 +55,6 @@
 
   // Windows Live Writerの編集用URLの非表示
   remove_action('wp_head', 'wlwmanifest_link');
-
-  // Admin Bar の非表示
-  function disable_admin_bar() {
-    return false;
-  }
-  add_filter('show_admin_bar', 'disable_admin_bar');
   
   // 絵文字機能の削除
   function disable_emoji() {
@@ -118,47 +105,6 @@
   }
   add_action('do_faviconico', 'default_favicon_delete');
 
-  // 抜粋文字数の指定
-  function custom_excerpt_length($length) {   
-    return 60; 
-  }   
-  add_filter('excerpt_length', 'custom_excerpt_length');
-
-  // 抜粋文末文字の指定
-  function custom_excerpt_more($more) {
-    return '…';
-  }
-  add_filter('excerpt_more', 'custom_excerpt_more');
-
-  // パスワード保護投稿の抜粋文変更
-  function excerpt_password_protected($excerpt) {
-    if(post_password_required()) {
-      $excerpt = 'この投稿はパスワードで保護されています';
-    }
-    return $excerpt;
-  }
-  add_filter('the_excerpt', 'excerpt_password_protected');
-
-  // パスワード保護投稿の入力フォーム導入文変更
-  function description_password_protected($text) {
-    $data_set = array(
-      'このコンテンツはパスワードで保護されています。閲覧するには以下にパスワードを入力してください。' => 'このコンテンツの閲覧はパスワードが必要です',
-      'パスワード: ' => '【パスワード】',
-      '確定' => '送信'
-    );
-    $search = array_keys($data_set);
-    $replace = array_values($data_set);
-    $text = str_replace($search, $replace, $text);
-    return $text;
-  }
-  add_filter('the_password_form', 'description_password_protected');
-
-  // パスワード保護投稿のタイトルプレフィックス変更
-  function prefix_password_protected() {
-    return '<span>【パスワード保護】</span>%s';
-  }
-  add_filter('protected_title_format', 'prefix_password_protected');
-
   // 自動整形機能（wpautop）設定
   remove_filter('the_excerpt', 'wpautop');
 
@@ -174,47 +120,5 @@
     exit();
   }
   add_action('wp_logout','redirect_logout_page');
-
-  // ユーザープロフィール情報追加
-  function add_user_profile($userProfile) {
-    $userProfile['Facebook'] = __('Facebookページ');
-    $userProfile['Twitter'] = __('Twitterページ');
-    return $userProfile;
-  }
-  add_filter('user_contactmethods', 'add_user_profile');
-
-  // 最終更新日の出力
-  function post_modified($date) {
-    $modifiedDate = get_the_modified_time('U');
-    $postDate = get_the_time('U');
-    if ($modifiedDate < $postDate) {
-      return get_the_time($mod_date);
-    } else if($modifiedDate === $postDate) {
-      return get_the_modified_time($date);
-    } else {
-      return get_the_modified_time($date);
-    }
-  }
-
-  // 固定ページの親子ページチェック
-  function has_parent_page() {
-    global $post;
-    if(is_page() && $post->post_parent) {
-      return $post->post_parent;
-    } else {
-      return false;
-    }
-  }
-
-  // 日付別アーカイブのチェック
-  function is_date_archive() {
-    if(get_query_var('year') && get_query_var('monthnum')) {
-      return true;
-    }
-  }
-  
-  // プラグイン・テーマの自動更新設定
-  add_filter('auto_update_plugin', '__return_true');
-  add_filter('auto_update_theme', '__return_true');
 
 ?>
